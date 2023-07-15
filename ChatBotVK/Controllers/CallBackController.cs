1ec741d78ba38fe657f58b57acc6a74bc6ac44bf
las <las@DESKTOP-AE7EQ1G>
2023-07-15 10:07:23 +0700

    Добавьте файлы проекта.

---
+++ ChatBotVK/Controllers/CallBackController.cs
@@ -0,0 +1,26 @@
+﻿using ChatBotVK.Models.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatBotVK.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallBackController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Callback(UpdateDto update)
+        {
+            // Проверяем, что находится в поле "type" 
+            switch (update.Type)
+            {
+                // Если это уведомление для подтверждения адреса
+                case "confirmation":
+                    // Отправляем строку для подтверждения 
+                    return Ok("355d55c7");
+            }
+            // Возвращаем "ok" серверу Callback API
+            return Ok("ok");
+        }
+    }
+}
---
+++ ChatBotVK/Models/Dtos/UpdateDto.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Nodes;
+
+namespace ChatBotVK.Models.Dtos
+{
+    [Serializable]
+    public class UpdateDto
+    {
+        /// <summary>
+        /// Тип события
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Объект, инициировавший событие
+        /// Структура объекта зависит от типа уведомления
+        /// </summary>
+        public JsonObject Object { get; set; }
+
+        /// <summary>
+        /// ID сообщества, в котором произошло событие
+        /// </summary>
+        public long GroupId { get; set; }
+    }
+}
